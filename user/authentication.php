f1d48094416d375f7b66484ed485c6b625590c39
TORRES MARC JUSTINE <2201775@slu.edu.ph>
2021-12-21 09:12:58 +0800

    authenticated!!!

--- user/authentication.php
+++ user/authentication.php
@@ -14,15 +14,18 @@
         $sql = "select *from users where username = '$username' and password = '$password'";  
         $result = mysqli_query($con, $sql);  
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);  
-          
-        if($row["type"]=="member"){  
-            $_SESSION['user_id'] = $user_id;
-            header("location: home.php");
-            
-            
-        }  
-        else{  
-            header("location: ../admin/admin.php");
-        }     
+        $count = mysqli_num_rows($result); 
+        
+        if($count == 1){
+            if($row["type"]=="member"){  
+                $_SESSION['user_id'] = $user_id;
+                header("location: home.php");
+             
+            }else{
+                header("location: ../admin/admin.php");
+            }
+        }else{
+                header("location: login.php");
+        }
+        
 ?>  
