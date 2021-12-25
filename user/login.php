5aca9fcfacf0cc55f0f01d96323930f868e67745
iJerms <2201341@slu.edu.ph>
2021-12-25 21:37:50 +0800

    modify login.css and login.php

--- user/login.php
+++ user/login.php
@@ -1,53 +1,59 @@
 <?php
-  require_once '../admin/connection.php'; 
+require_once '../admin/connection.php';
 session_start();
-//authentication.php
+// authentication.php
 ?>
-<html>  
-<head>  
-    <title> Login Form</title>  
-   
-    <link rel = "stylesheet" type = "text/css" href = "../styles/login.css">   
-</head>  
-<body>  
-    <div id = "frm">  
-        <h1>Login</h1>  
-        <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
-            <p>  
-                <label> Username: </label>  
-                <input type = "text" id ="user" name  = "user" />  
-            </p>  
-            <p>  
-                <label> Password: </label>  
-                <input type = "password" id ="pass" name  = "pass" />  
-            </p>  
-            <p>     
-                <input type =  "submit" id = "btn" value = "Login" />  
-            </p>  
-        </form>  
-    </div>  
-    
-    <script>  
-            function validation()  
-            {  
-                var id=document.f1.user.value;  
-                var ps=document.f1.pass.value;  
-                if(id.length=="" && ps.length=="") {  
-                    alert("User Name and Password fields are empty");  
-                    return false;  
-                }  
-                else  
-                {  
-                    if(id.length=="") {  
-                        alert("User Name is empty");  
-                        return false;  
-                    }   
-                    if (ps.length=="") {  
-                    alert("Password field is empty");  
-                    return false;  
-                    }  
-                }                             
-            }  
-        </script>  
-</body>     
-</html>  
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles/login.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <div id="form">
+        <h1>Welcome to MakiConcert!</h1>
+        <form name="f1" action="authentication.php" onsubmit="return validation()" method="POST">
+            <div>
+                <label>Username:</label>
+                <input type="text" id="user" name="user" />
+                <br>
+            </div>
+            <br>
+            <div>
+                <label>Password: </label>
+                <input type="password" id="pass" name="pass" />
+                <br>
+            </div>
+            <br>
+            <input type="submit" id="btn" value="Login" />
+        </form>
+    </div>
+
+    <script>
+        function validation() {
+            var id = document.f1.user.value;
+            var ps = document.f1.pass.value;
+            if (id.length == "" && ps.length == "") {
+                alert("Username and Password fields are empty!");
+                return false;
+            } else {
+                if (id.length == "") {
+                    alert("Username field is empty!");
+                    return false;
+                }
+                if (ps.length == "") {
+                    alert("Password field is empty!");
+                    return false;
+                }
+            }
+        }
+    </script>
+</body>
+
+</html>
