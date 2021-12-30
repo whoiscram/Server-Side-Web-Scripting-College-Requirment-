43cd41ae7b3d992a7c7a5d4885e2937db2672631
TORRES MARC JUSTINE <2201775@slu.edu.ph>
2021-12-30 14:09:14 +0800

    modifed db

--- maki.sql
+++ maki.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2021 at 06:00 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- Generation Time: Dec 30, 2021 at 07:07 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,28 +45,14 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`id`, `title`, `performer`, `venue`, `description`, `date_start`, `date_end`, `ticket_price`, `status`, `created_at`) VALUES
-(1, 'OPM ARTISTS', 'Parokya ni Edgar,Imago, Moonstar88, 6cyclemind', 'Araneta Coliseum', 'dami nilang kakanta', '2021-12-21 18:00:00', '2021-12-22 18:00:00', 5000, 'Ongoing', '2021-12-16 18:00:00'),
-(2, 'Awit Para Sa Mga Bata', 'Lea Salonga', 'Kia Theatre', 'Awit para sayo', '2021-12-17 13:00:00', '2021-12-17 16:00:00', 0, '550', '2021-12-17 03:00:00'),
-(10, 'Hiraya', 'Cup of Joe', 'CCA Theatre', 'Kakantahin ng cup of joe yung Hiraya sa CCA Theatre', '2021-12-25 17:00:00', '2021-12-25 19:30:00', 0, '250', '2021-12-18 17:00:00'),
-(11, 'World Tour', 'TWICE', 'MOA', 'is sana gae', '2021-12-20 06:00:00', '2021-12-23 20:00:00', 1000, 'Ongoing', '2021-12-20 17:54:18'),
-(13, 'sample', 'sample', 'sample', 'sample', '2021-12-21 02:10:00', '2021-12-22 02:10:00', 100, 'sample', '2021-12-21 02:10:22'),
-(14, 'asdf', 'aasdf', 'asdf', 'asdf', '2021-12-22 11:52:00', '2021-12-22 11:52:00', 12, 'Done', '2021-12-21 08:27:31'),
-(15, 'asdf', 'a', 'asdf', 'asdf', '2021-12-01 11:40:00', '2021-12-08 11:40:00', 1, 'dasf', '2021-12-21 11:40:13'),
-(16, 'asdf', 'a', 'asdf', 'asdf', '2021-12-01 11:40:00', '2021-12-08 11:40:00', 1, 'dasf', '2021-12-21 11:41:27'),
-(17, 'asd', 'asd', 'asd', 'asd', '2021-12-09 11:41:00', '2021-12-17 11:41:00', 1232, 'asdasd', '2021-12-21 11:41:40'),
-(18, 'sadf', 'asdf', 'asdf', 'asdf', '2021-12-22 11:45:00', '2021-12-22 11:45:00', 1334, 'adfasdf', '2021-12-21 11:45:24'),
-(19, 'asdf', 'asdf', 'asdf', 'asdf', '2021-12-21 11:45:00', '2021-12-16 11:45:00', 0, 'asdf', '2021-12-21 11:45:34'),
-(22, 'Concert', 'Maki', 'MOA', 'Singing ', '2021-12-21 13:38:00', '2021-12-25 13:38:00', 1000, 'Done', '2021-12-21 13:36:55'),
-(23, '', '', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '', '2021-12-21 13:45:44'),
-(24, '<h1>Programming</h1>', '<h1>maki</h1>', '<h1>samcis</h1>', '<h1>demo</h1>', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '', '2021-12-21 13:47:12'),
-(25, 'digital arts', 'maki', 'samcis', 'demo', '2021-12-01 13:49:00', '2021-11-01 13:49:00', 0, '', '2021-12-21 13:49:31'),
-(26, '', '', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '', '2021-12-21 13:50:57'),
-(28, 'adsfcsrd', 'eargfer', 'esrgverfgv', 'ergverfv', '2021-12-30 00:15:00', '2021-12-08 00:15:00', 455, 'Ongoing', '2021-12-28 00:15:15'),
-(29, 'wsefewsr', 'wergerfg', 'ergverf', 'ergerfg', '2021-12-09 00:19:00', '2021-12-01 00:18:00', 5666, 'Upcoming', '2021-12-28 00:18:21'),
-(30, 'hat', 'dog', 'gaerg', 'ergertf', '2021-12-07 14:38:00', '2021-12-15 17:42:00', 5600, 'Upcoming', '2021-12-28 00:37:34'),
-(31, 'hatdig', 'hatdig', 'aferg', 'eargerg', '2021-12-07 23:27:00', '2021-12-16 23:27:00', 25, 'Finished', '2021-12-28 23:27:56'),
-(32, 'sferg', 'etrgderg', 'ertge5rg', 'etrgerg', '2021-11-04 23:28:00', '2021-11-07 23:28:00', 123, 'Finished', '2021-12-28 23:28:19'),
-(33, 'sdegferg', 'ergsrdetg', 'tderbhrfgth', 'gyhrdfth', '2021-12-09 23:28:00', '2021-12-31 23:28:00', 4854, 'Finished', '2021-12-28 23:28:45');
+(1, 'OPM ARTISTS', 'Parokya ni Edgar,Imago, Moonstar88, 6cyclemind', 'Araneta Coliseum', 'dami nilang kakanta', '2021-12-21 18:00:00', '2021-12-22 18:00:00', 5000, 'Finished', '2021-12-16 18:00:00'),
+(2, 'Awit Para Sa Mga Bata', 'Lea Salonga', 'Kia Theatre', 'Awit para sayo', '2021-12-17 13:00:00', '2021-12-17 16:00:00', 0, 'Finished', '2021-12-17 03:00:00'),
+(3, 'Hiraya', 'Cup of Joe', 'CCA Theatre', 'Kakantahin ng cup of joe yung Hiraya sa CCA Theatre', '2023-01-01 08:00:00', '2022-01-01 10:00:00', 100, 'Upcoming', '2021-12-30 13:44:52'),
+(4, 'World Tour', 'TWICE', 'MOA', 'is sana gae', '2021-12-20 06:00:00', '2021-12-23 20:00:00', 1000, 'Cancelled', '2021-12-20 17:54:18'),
+(5, 'Arthur\'s Cause', 'Arthur Nery', 'Casa Generosa Building 86 Upper Gen. Luna Road Extention Brgy, Baguio, 2600 Benguet', 'Arthurs Philippine Tour', '2023-01-05 10:00:00', '2023-01-05 12:00:00', 500, 'Upcoming', '2021-12-30 13:52:14'),
+(6, 'Because its for a Cause', 'Because, Kiyo , Shorttone and Al James', '33 Legarda Rd, Barangay Burnham - Legarda, Baguio, 2600 Benguet', '4 local rappers are going to sing and rap for a cause', '2022-01-06 12:00:00', '2022-01-06 13:00:00', 500, 'Cancelled', '2021-12-30 13:52:14'),
+(7, 'Ben and ben and ben and ben', 'Ben & Ben', '#39 Tan Building, Lower Session Road, Baguio, 2600 Benguet', 'Open Mic for aspiring singers held by the band\r\nBen & Ben', '2022-01-01 15:00:00', '2022-01-01 16:00:00', 500, 'Ongoing', '2021-12-30 13:59:06'),
+(8, 'All for one we as ONE', 'Juan Carlos, SUD, Up Dharma Down', 'SM City Baguio Luneta Hill, Upper Session Rd, Baguio, 2600 Benguet', 'Join and be one of the most underrated singers and bands ', '2022-01-06 15:20:00', '2022-01-06 18:20:00', 450, 'Ongoing', '2021-12-30 13:59:06');
 
 -- --------------------------------------------------------
 
@@ -92,7 +78,15 @@ INSERT INTO `event_users` (`user_id`, `event_id`, `type`) VALUES
 (4, 1, 'member'),
 (4, 2, 'member'),
 (4, 3, 'member'),
-(13, 0, '');
+(11, 5, 'member'),
+(11, 8, 'member'),
+(14, 7, 'member'),
+(14, 8, 'member'),
+(15, 5, 'member'),
+(16, 3, 'member'),
+(16, 5, 'member'),
+(17, 3, 'member'),
+(18, 7, 'member');
 
 -- --------------------------------------------------------
 
@@ -129,8 +123,11 @@ INSERT INTO `users` (`user_id`, `username`, `given_name`, `surname`, `phone_numb
 (10, 'dambledoor', 'Albus', 'Dumbledore', '09274411995', 'admin', 'elderwand@gmail.com', 'kedavra', '2021-05-09 00:00:00'),
 (11, 'marc', 'Marc Justine', 'Torres', '09994580043', 'member', 'torreshehe@gmail.com', 'cram', '2021-12-21 12:52:08'),
 (12, 'maki', 'maki', 'maki', 'maki', 'admin', 'maki@maki.com', 'maki', '2021-12-21 12:52:31'),
-(13, 'user1', 'user1', 'user1', '444241414', 'member', 'user1@gmail.com', 'user1', '2021-12-21 12:53:51'),
-(14, 'smrej', 'Jermel Mari', 'Danganan', '09398444128', 'member', 'jm.danganan08@gmail.com', 'smrej', '2021-12-24 14:30:00');
+(14, 'smrej', 'Jermel Mari', 'Danganan', '09398444128', 'member', 'jm.danganan08@gmail.com', 'smrej', '2021-12-24 14:30:00'),
+(15, 'chey', 'Cheynadine Marlene', 'Dauz', '0997333394', 'member', 'cheynadine@gmail.com', 'chey', '2021-12-30 13:32:41'),
+(16, 'luwy', 'Luwy', 'Colina', '0998924346', 'member', 'luwycol@gmail.com', 'luwy', '2021-12-30 13:32:41'),
+(17, 'sheena', 'Sheena ', 'Costales', '0997349158', 'member', 'sheenacos@gmail.com', 'sheena', '2021-12-30 13:34:20'),
+(18, 'yverre', 'Yverre John', 'Cabintos', '0998131309', 'member', 'yverrecab@gmail.com', 'yverre', '2021-12-30 13:34:20');
 
 --
 -- Indexes for dumped tables
@@ -162,13 +159,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
